7a31e5bbe4c0276b2f51f1a26261f34cbee027d3
DianaPoint <151085171+DianaPoint@users.noreply.github.com>
2023-11-16 22:42:32 +0100

    Create app.js

---
+++ scripts/app.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', function () {
+   const menuIcon = document.getElementById('menu-icon');
+   const navLinks = document.querySelector('.nav');
+   const slider = document.querySelector('.slider');
+   const controls = document.querySelectorAll('.control');
+   const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+
+   menuIcon.addEventListener('click', function () {
+       navLinks.classList.toggle('show');
+   });
+
+   let currentIndex = 0;
+
+   function showSlide(index) {
+       const percentage = index * -100;
+       slider.style.transform = `translateX(${percentage}%)`;
+   }
+
+   function nextSlide() {
+       currentIndex = (currentIndex + 1) % slider.children.length;
+       showSlide(currentIndex);
+       updateControls();
+   }
+
+   function prevSlide() {
+       currentIndex = (currentIndex - 1 + slider.children.length) % slider.children.length;
+       showSlide(currentIndex);
+       updateControls();
+   }
+
+   prevButton.addEventListener('click', prevSlide);
+   nextButton.addEventListener('click', nextSlide);
+
+ controls.forEach((control, index) => {
+      control.addEventListener('click', function () {
+          currentIndex = index;
+          showSlide(currentIndex);
+          updateControls();
+      });
+  });
+  function updateControls() {
+   controls.forEach((control, index) => {
+       control.classList.toggle('active', index === currentIndex);
+});
+  }
+});
